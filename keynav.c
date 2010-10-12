8cf995536d677b6ce5438f19e1169458a4d2b59c
Jordan Sissel <jls@semicomplete.com>
2010-10-12 23:13:27 +0000

    - revert, accidental commit
    git-svn-id: https://semicomplete.googlecode.com/svn/keynav@3052 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- keynav.c
+++ keynav.c
@@ -507,6 +507,7 @@ int parse_config_line(char *orig_line) {
 
   tokctx = line;
   keyseq = strdup(strtok_r(line, " ", &tokctx));
+  commands = strdup(tokctx);
 
   /* A special config option that will clear all keybindings */
   if (strcmp(keyseq, "clear") == 0) {
